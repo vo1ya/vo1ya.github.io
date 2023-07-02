025082dfe4ec86c4ad797e38d1108c4412edaccc
OlgaValiamova <olga.pozyubanova@gmail.com>
2023-07-02 18:05:25 +0500

    122

--- countries/js/app.js
+++ countries/js/app.js
@@ -1,57 +1,30 @@
-// <li class="tile__item">
-            
-// https://restcountries.com/v3.1/lang/spanish
-
-
-// const _apiBase = 'https://restcountries.com/v3.1/lang';
-
-// const getResource = async (url) => {
-// const res = await fetch(`${this._apiBase}${url}`);
-
-// if (!res.ok) {
-//     throw new Error(`Could not fetch ${url}` +
-//     `, received ${res.status}`)
-// }
-// return await res.json();
-// };
-
-
-
-// const _transformCountry = (country) => {
-//     return {
-//       id: country.id,
-//       name: country.name,
-//       capital: country.captial,
-//       flag: country.flag
-//     };
-//   };
-
-
-// const getCountries = async () => {
-//     const res = await this.getResource(`/${country}/`);
-//     return res.results.map(this._transformCountry);
-//   };
-
-
-
-
-
-
-
-const baseUrl = 'https://restcountries.com/v3.1/lang/rus';
+const baseUrl = 'https://restcountries.com/v3.1/lang/';
 
 let countriesList = document.querySelector('.tile');
 
+document.querySelectorAll('.lang-list__item').forEach(item => {
+  if (document.querySelectorAll('.lang-list__item')) {
+      const node = document.querySelector('.tile');
 
+      const list = document.querySelectorAll('.lang-list__item')
+      list.forEach(item =>{ 
+             item.addEventListener('click', (e) =>{
+             list.forEach(el=>{ el.classList.remove('active'); });
+             item.classList.add('active')
+             const langId = item.id;
+             console.log(langId)
+             getCountries(e.target.id)
+         })
+     })
+    }})
 
-
-function getCountries(lang) {
-    fetch(baseUrl)
+function getCountries(langId) {
+    fetch(baseUrl + langId)
     .then((response) => {
       return response.json();
     })
     .then((data) => {
-    console.log(data[0].name['common'])
+    // console.log(data[0].name['common'])
 
       let countries = data;
 
@@ -71,18 +44,6 @@ function getCountries(lang) {
     });
 }
 
-document.querySelectorAll('.lang-list').forEach(item => {
-    if (document.querySelectorAll('.lang-list')) {
-        const node = document.querySelector('.tile');
-        // node.parentNode.removeChild(node);
 
-        item.addEventListener('click', e => {
-            e.target.classList.add('active');
-            getCountries(e.target.id);
-        });
-    }
-    
-});
 
-    
-getCountries('spanish');
+getCountries(langId);
